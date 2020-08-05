0e66e7b7e64531ca2ac05f879b5d92f6a7e2ba1c
Federico Agarinis <f.agarinis@solvingteam.it>
2020-08-05 17:02:26 +0200

    nuovi

---
+++ src/thread/ParallelStreamTest.java
@@ -0,0 +1,12 @@
+package thread;
+
+import java.util.List;
+
+public class ParallelStreamTest {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(2, 3,2 );
+        int i = list.parallelStream().reduce(1, Integer::sum, (i1, i2) -> i1 * i2);
+        System.out.println(i);
+
+    }
+}
