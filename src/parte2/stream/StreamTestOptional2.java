c47e1a10bed5d0d4d69abc11888f2299044408b2
Federico Agarinis <f.agarinis@solvingteam.it>
2020-08-18 11:14:43 +0200

    stream e function

--- src/parte2/stream/StreamTestOptional2.java
+++ src/parte2/stream/StreamTestOptional2.java
@@ -8,5 +8,14 @@ public class StreamTestOptional2 {
     public static void main(String[] args) {
         // stampa Optional.empty
         System.out.println(Stream.of(Optional.ofNullable(null)).findFirst().get());
+
+
+        //crea stream
+        Stream stream = Stream.ofNullable("ciao");
+        Stream<String> stream2 = Stream.<String>builder().add("a").build();
+        Stream<Double> stream3 = Stream.generate(() -> Math.random()); //infinite stream provided by supplier
+
+        stream3.limit(10).forEach(n-> System.out.println(n)); //senza limit andrebbe all'infinito
+
     }
 }
