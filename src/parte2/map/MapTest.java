bc08278e3e95db2c60b9aa2eedc179db8921e257
Federico Agarinis <f.agarinis@solvingteam.it>
2020-09-02 13:26:31 +0200

    fix

--- src/parte2/map/MapTest.java
+++ src/parte2/map/MapTest.java
@@ -13,7 +13,7 @@ public class MapTest {
         map.compute("Jane", (s1, s2) -> "Hi " + s2);
         System.out.println(map);
 
-        map.computeIfAbsent("Johnson", key -> "oh long " + key);
+        String result = map.computeIfAbsent("Johnson", key -> "oh long " + key); //returna il risultato della funzione
         System.out.println("Map after computeIfAbsent");
         System.out.println(map);
     }
