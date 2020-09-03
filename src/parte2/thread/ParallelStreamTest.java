732837dc15e3aa8c7fbd9fb4822f2c015f5f5573
Federico Agarinis <f.agarinis@solvingteam.it>
2020-09-03 12:44:27 +0200

    nuovi

--- src/parte2/thread/ParallelStreamTest.java
+++ src/parte2/thread/ParallelStreamTest.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 public class ParallelStreamTest {
     public static void main(String[] args) {
-        List<Integer> list = List.of(2, 3,2 );
-        int i = list.parallelStream().reduce(1, Integer::sum, (i1, i2) -> i1 * i2);
+        List<Integer> list = List.of(2, 3, 2);
+        // the identity must be an identity for the combiner function
+        int i = list.parallelStream().reduce(1, Integer::sum, (i1, i2) -> i1 * i2); // parallel, combiner used
+        int j = list.stream().reduce(1, Integer::sum, (i1, i2) -> i1 * i2); // sequential, combiner not used
         System.out.println(i);
+        System.out.println(j);
 
     }
 }
